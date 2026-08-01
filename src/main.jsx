8d90d355512a34a9abd680d74334edc05ef02ca6
johngabriel903 <37164345+johngabriel903@users.noreply.github.com>
2026-08-01 09:01:45 -0700

    Update pic

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { ArrowRight, FlaskConical, BookOpen, Shield, Brain, Crown } from "lucide-react";
 import "./styles.css";
-import productHero from "../2ndproduct-hero.v2.png";
+import productHero from "../product-herov2.final.png";
 
 const pages = {
   home: {
